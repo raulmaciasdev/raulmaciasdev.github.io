e8ad7e6c6955f33002b7d181fd0f3af41f2d203e
raulmaciasdev <raulmaciasdev@gmail.com>
2025-08-22 16:49:45 +0200

    update_avatar

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/raulmaciasdev.github.io/' // 👈 muy importante
-}))
+  base: '/raulmaciasdev.github.io/' // 👈 muy importante
+})
