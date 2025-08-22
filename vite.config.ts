c48ce7172d2896a8dad17de7a0fbf7f0fa71bcdb
raulmaciasdev <raulmaciasdev@gmail.com>
2025-08-22 16:52:04 +0200

    ruta_relativa

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/raulmaciasdev.github.io/' // 👈 muy importante
+  base: './' // 👈 muy importante
 })
